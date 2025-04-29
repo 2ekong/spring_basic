30825a217a59d1db356b7f69f1a8f507f0ca200c
2ekong <89740700+2ekong@users.noreply.github.com>
2025-04-29 18:10:27 +0900

    Add files via upload

---
+++ sprweb22ajaxdb/src/main/java/pack/model/DataDao.java
@@ -0,0 +1,28 @@
+package pack.model;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DataDao {
+	@Autowired
+	private DataRepository dataRepository;
+
+	public List<SangpumDto> getSangpumAll() {
+		/* 람다 표현식
+		List<SangpumDto> slist = dataRepository.findAll().stream().map(sangpum -> SangpumDto.builder()
+				.code(sangpum.getCode())
+				.sang(sangpum.getSang())
+				.su(sangpum.getSu())
+				.dan(sangpum.getDan())
+				.build())
+				.collect(Collectors.toList());
+		*/
+		
+		List<SangpumDto> slist = dataRepository.findAll().stream().map(SangpumDto :: toDto).collect(Collectors.toList());
+		return slist;
+	}
+}
---
+++ sprweb22ajaxdb/src/main/java/pack/model/SangpumDto.java
@@ -0,0 +1,28 @@
+package pack.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SangpumDto {
+	private double code;
+	private String sang;
+	private String su;
+	private String dan;
+	
+	public static SangpumDto toDto(Sangpum sangpum) {	//Entity -> Dto
+		return SangpumDto.builder()
+				.code(sangpum.getCode())
+				.sang(sangpum.getSang())
+				.su(sangpum.getSu())
+				.dan(sangpum.getDan())
+				.build();
+	}
+}
