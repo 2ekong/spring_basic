60a0900a798d466fc47a4155dec198256264e945
2ekong <89740700+2ekong@users.noreply.github.com>
2025-04-29 10:08:22 +0900

    Add files via upload

---
+++ sprweb21ajax/src/main/java/pack/MyModel.java
@@ -0,0 +1,11 @@
+package pack;
+
+import org.springframework.stereotype.Component;
+import lombok.Data;
+
+@Component
+@Data
+public class MyModel {
+	private String name;
+	private String skills[];
+}
